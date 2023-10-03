e57d1a5e4cb1cf9d6c3650a824d1d53841d783d1
aymane taoufyq <skays.dev@gmail.com>
2023-10-03 13:14:36 +0100

    g

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in 'abcdefghijklmnopqrstuvwxyz':
-    if letter not in 'qe':
-        print(letter, end='')
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
