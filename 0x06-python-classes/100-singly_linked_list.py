fe1d2a6a32016e6900f393754a0b21a874291cd3
aymane taoufyq <skays.dev@gmail.com>
2023-10-24 16:08:12 +0100

    g

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -16,7 +16,7 @@ class Node:
     @property
     def data(self):
         """Return data stored in the node."""
-        return self.__data
+        return (self.__data)
 
     @data.setter
     def data(self, value):
@@ -35,7 +35,7 @@ class Node:
     @property
     def next_node(self):
         """Return next node in the singly linked list."""
-        return self.__next_node
+        return (self.__next_node)
 
     @next_node.setter
     def next_node(self, value):
@@ -50,3 +50,46 @@ class Node:
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
+
+class SinglyLinkedList:
+    """Define a singly linked list."""
+
+    def __init__(self):
+        """Constructor."""
+        self.__head = None
+
+    def __str__(self):
+        """Print the singly linked list."""
+        string = ""
+        node = self.__head
+        while node:
+            string += str(node.data)
+            if node.next_node:
+                string += "\n"
+            node = node.next_node
+        return (string)
+
+    def sorted_insert(self, value):
+        """Insert a new Node into the correct sorted position in the list.
+
+        Args:
+            value (int): Value of the new node.
+
+        Raises:
+            TypeError: If value is not an integer.
+        """
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        new_node = Node(value)
+        if not self.__head:
+            self.__head = new_node
+            return
+        if value < self.__head.data:
+            new_node.next_node = self.__head
+            self.__head = new_node
+            return
+        node = self.__head
+        while node.next_node and value > node.next_node.data:
+            node = node.next_node
+        new_node.next_node = node.next_node
+        node.next_node = new_node
