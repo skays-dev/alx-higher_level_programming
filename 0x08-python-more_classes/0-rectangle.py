32b26f49beb87c125abe6dc9d6318fe2a475d2a4
aymane taoufyq <skays.dev@gmail.com>
2023-10-31 13:55:29 +0100

    go

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Defining an empty class Rectangle"""
-
 class Rectangle:
-"""Empty representation of a Rectangle"""
-pass
+    """Empty representation of a Rectangle"""
+    pass
